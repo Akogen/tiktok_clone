46adde010783596404ecc1cab013525c9bbea8db
akogenweb <akogenweb@gmail.com>
2023-04-22 12:47:48 -0700

    5.1 Interests Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
-import 'package:tiktok_clone/features/authentication/sign_up_screen.dart';
+import 'package:tiktok_clone/features/onboarding/interests_screen.dart';
 
 void main() {
   runApp(const TiktokApp());
@@ -28,7 +28,7 @@ class TiktokApp extends StatelessWidget {
           centerTitle: true,
         ),
       ),
-      home: const SignUpScreen(),
+      home: const InterestsScreen(),
     );
   }
 }
